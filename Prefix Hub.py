a5fb9557eb04db4079b1be8ae2e39b8bdd83774a
b9na <111341708+b9natwo@users.noreply.github.com>
2023-08-20 20:40:07 -0700

    Create Prefix Hub.py

---
+++ Prefix Hub.py
@@ -0,0 +1,308 @@
+import discord
+import asyncio
+import requests
+from discord.ext import commands
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import datetime
+import pytz
+import os
+import json
+import random
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="!", help_command=None, intents=intents)
+
+# Variables Needed For Change
+
+leaks_channel_id = None # Where the bot will send the leaks
+error_channel_id = None # Insert a Channel Id for the bot to send errors to (in case something goes wrong)
+Email = None # For Login Purposes
+Password = None # For Login Purposes
+TOKEN = None # Discord Bot Token
+
+leak_channels_guilds = {}
+
+def login_to_website(email, password):
+    # Set up Selenium WebDriver with appropriate options
+    options = Options()
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('-headless')
+    driver = webdriver.Firefox(options=options)
+
+    # Load the login page and fill in the credentials
+    driver.get("https://leaked.cx/login")
+    driver.find_element(By.NAME, "login").send_keys(email)
+    driver.find_element(By.NAME, "password").send_keys(password)
+    driver.find_element(By.XPATH, "/html/body/div[2]/div/div[4]/div/div/div/div/div/div/form/div[1]/dl/dd/div/div[2]/button/span").click()
+    # Wait for the login process to complete
+    driver.implicitly_wait(10)
+    return driver
+
+async def scrape_leaks():
+    global error_channel
+
+    try: # All Leaks
+                # Log into the website
+        driver = login_to_website(f"{Email}", f"{Password}")
+
+        # Navigate to the Juice WRLD leaks section
+        driver.get("https://leaked.cx/forums/hiphopleaks/")
+
+        # Wait for the page to load
+        await asyncio.sleep(5)
+
+        # Scrape the page for new leaks
+        soup = BeautifulSoup(driver.page_source, "html.parser")
+        leaks = soup.find_all("div", class_="structItemContainer-group js-threadList")
+
+        # Iterate through the leaks and send Discord embed messages
+        for leak in leaks:
+            leak_title = leak.find("a", attrs = {"class" : ""}).text.strip()
+            leak_url = leak.find("a", attrs= {"class" : ""})["href"]
+            leak_datetime = leak.find("time", attrs={"class": "u-dt"}).text.strip()
+            leak_tag = leak.find("span", attrs={"dir": "auto"}).text.strip()
+
+            if leak_tag == "LEAK":
+                leak_title = leak.find("a", attrs = {"class" : ""}).text.strip() + " [LEAK]"
+
+            if leak_tag == "EARLY":
+                leak_title = leak.find("a", attrs = {"class" : ""}).text.strip() + " [EARLY]"
+
+            if leak_tag == "SNIPPET":
+                leak_title = leak.find("a", attrs = {"class" : ""}).text.strip() + " [SNIPPET]"
+
+            if leak_tag == "DEMO":
+                leak_title = leak.find("a", attrs = {"class" : ""}).text.strip() + " [DEMO]"
+
+            if leak_tag == "OLD LEAK":
+                leak_title = leak.find("a", attrs = {"class" : ""}).text.strip() + " [OLD LEAK]"
+
+            # Check if the leak has already been sent
+            if not is_duplicate(leak_title):
+                # Get a random Juice WRLD image online as the thumbnail
+                thumbnail_url = None
+
+                role_id = " "
+
+                get_section = leak.find("div", attrs={"class":"structItem-title"})
+
+                get_tags = get_section.find_all("span", attrs={"dir":"auto"})
+
+                for tag in get_tags:
+                    k = tag.get_text()
+                    if k == "JUICE WRLD":
+                        role_id = None # Juice Role (For Pinging Purposes)
+                        thumbnail_url = get_random_juice_wrld_image()
+                    if k == "LIL UZI VERT":
+                        role_id = None # Uzi Role (For Pinging Purposes)
+                        thumbnail_url = get_random_lil_uzi_vert_image()
+                    if k == "KANYE WEST":
+                        role_id = None # Kanye Role (For Pinging Purposes)
+                        thumbnail_url = get_random_kanye_west_image()
+                    if k == "TRAVIS SCOTT":
+                        thumbnail_url = get_random_travis_scott_image()
+                        role_id = None # Travis Role (For Pinging Purposes)
+                    if k == "THE WEEKND":
+                        role_id = None # Weeknd Role (For Pinging Purposes)
+                        thumbnail_url = get_random_the_weeknd_image()
+                    if k == "PLAYBOI CARTI":
+                        role_id = None # Carti Role (For Pinging Purposes)
+                        thumbnail_url = get_random_playboi_carti_image()
+                    if k == "DRAKE":
+                        role_id = None # Drake Role (For Pinging Purposes)
+                        thumbnail_url = get_random_drake_image()
+                    if k == "LIL WAYNE":
+                        thumbnail_url = get_random_lil_wayne_image()
+                        role_id = None # Wayne Role (For Pinging Purposes)
+                    if k == "YOUNG THUG":
+                        thumbnail_url = get_random_young_thug_image()
+                        role_id = None # Thug Role (For Pinging Purposes)
+                    if k == "MAC MILLER":
+                        thumbnail_url = get_random_mac_miller_image()
+                        role_id = None # Mac Miller Role (For Pinging Purposes)
+                    if k == "MIGOS":
+                        thumbnail_url = get_random_migos_image()
+                        role_id = None # Migos Role (For Pinging Purposes)
+
+                    if "Yeat" in leak_title: # FOR CUSTOM PREFIX PURPOSES, THIS IS WHAT YOU'LL HAVE TO PUT.
+                        role_id = None # Yeat Role (For Pinging Purposes)
+                        thumbnail_url = get_random_yeat_image()
+
+                channel = bot.get_channel(leaks_channel_id)
+
+                embed = discord.Embed(title=leak_title, url=f"https://leaked.cx{leak_url}", color=discord.Color.random())
+                embed.set_thumbnail(url=thumbnail_url)
+                embed.set_footer(text=f"Leaked | {leak_datetime}")
+                
+                # Send the embed message to the specified channel
+                if role_id == " ":
+                    print("No Role")
+                else:
+                    await channel.send(f"<@&{role_id}>")
+                await channel.send(embed=embed)
+                # Store the sent leak to avoid duplicates
+                store_leak(leak_title)
+
+        # Close the Selenium WebDriver
+        driver.quit()
+
+    except Exception as e:
+        # Send the error message to the error channel
+        if error_channel is not None:
+            await error_channel.send(f"An error occurred while scraping leaks: {str(e)}")
+
+def is_duplicate(leak_title):
+    if os.path.exists("sent_leaks.json"):
+        with open("sent_leaks.json") as file:
+            sent_leaks = json.load(file)
+            return leak_title in sent_leaks
+    else:
+        return False
+
+def store_leak(leak_title):
+    if os.path.exists("sent_leaks.json"):
+        with open("sent_leaks.json") as file:
+            sent_leaks = json.load(file)
+    else:
+        sent_leaks = []
+
+    sent_leaks.append(leak_title)
+
+    with open("sent_leaks.json", "w") as file:
+        json.dump(sent_leaks, file)
+
+def get_random_juice_wrld_image():
+    # Implement code to fetch a random image of Juice WRLD from an online source
+    # Example:
+    image_urls = [
+        "https://t2.genius.com/unsafe/306x306/https%3A%2F%2Fimages.genius.com%2F42b0e9b115b685c8dd7e7f32e3f1ce9c.999x999x1.jpg"
+        # Add more image URLs as needed
+    ]
+    return random.choice(image_urls)
+
+def get_random_kanye_west_image():
+    # Implement code to fetch a random image of Juice WRLD from an online source
+    # Example:
+    image_urls = [
+        "https://images.genius.com/df2a9086c5f50a8615a4117513552f09.731x731x1.jpg"
+        # Add more image URLs as needed
+    ]
+    return random.choice(image_urls)
+
+def get_random_lil_uzi_vert_image():
+    # Implement code to fetch a random image of Juice WRLD from an online source
+    # Example:
+    image_urls = [
+        "https://images.genius.com/9cda5b801691bcd6559ab79ddf9c2925.1000x1000x1.jpg"
+        # Add more image URLs as needed
+    ]
+    return random.choice(image_urls)
+
+
+def get_random_travis_scott_image():
+    # Implement code to fetch a random image of Juice WRLD from an online source
+    # Example:
+    image_urls = [
+        "https://images.genius.com/5d19fecdb3828ca9ec89dda588e2eb7d.1000x1000x1.jpg"
+        # Add more image URLs as needed
+    ]
+    return random.choice(image_urls)
+
+def get_random_the_weeknd_image():
+    # Implement code to fetch a random image of Juice WRLD from an online source
+    # Example:
+    image_urls = [
+        "https://images.genius.com/1bab7f9dbd1216febc16d73ae4da9bd0.1000x1000x1.jpg"
+        # Add more image URLs as needed
+    ]
+    return random.choice(image_urls)
+
+def get_random_playboi_carti_image():
+    # Implement code to fetch a random image of Juice WRLD from an online source
+    # Example:
+    image_urls = [
+        "https://images.genius.com/988f153044ac8570914c1b42917d0db6.603x603x1.png"
+        # Add more image URLs as needed
+    ]
+    return random.choice(image_urls)
+
+def get_random_drake_image():
+    # Implement code to fetch a random image of Juice WRLD from an online source
+    # Example:
+    image_urls = [
+        "https://images.genius.com/7dbc2190892ad991a1abdd33c65990b6.623x623x1.jpg"
+        # Add more image URLS as needed
+    ]
+
+    return random.choice(image_urls)
+
+def get_random_lil_wayne_image():
+    # Implement code to fetch a random image of Juice WRLD from an online source
+    # Example:
+    image_urls = [
+        "https://images.genius.com/aa8b9dce2492fe413c23f77b643788fd.914x914x1.jpg"
+        # Add more image URLS as needed
+    ]
+
+    return random.choice(image_urls)
+
+def get_random_young_thug_image():
+    # Implement code to fetch a random image of Juice WRLD from an online source
+    # Example:
+    image_urls = [
+        "https://images.genius.com/4def6a8201847b0a918f2b12e4334725.760x760x1.jpg"
+        # Add more image URLS as needed
+    ]
+
+    return random.choice(image_urls)
+
+def get_random_mac_miller_image():
+    # Implement code to fetch a random image of Juice WRLD from an online source
+    # Example:
+    image_urls = [
+        "https://images.genius.com/904f5ed20e5f3bbf5391f178c55c3d01.700x700x1.jpg"
+        # Add more image URLS as needed
+    ]
+
+    return random.choice(image_urls)
+
+def get_random_yeat_image():
+    # Implement code to fetch a random image of Juice WRLD from an online source
+    # Example:
+    image_urls = [
+        "https://t2.genius.com/unsafe/340x340/https%3A%2F%2Fimages.genius.com%2F29e0cc0219f861c08132e9eaf4ec31de.640x640x1.jpg"
+        # Add more image URLS as needed
+    ]
+
+    return random.choice(image_urls)
+
+
+def get_random_migos_image():
+    # Implement code to fetch a random image of Juice WRLD from an online source
+    # Example:
+    image_urls = [
+        "https://images.genius.com/7fbac3bee19b97a9b5b2805512da490a.640x640x1.jpg"
+        # Add more image URLS as needed
+    ]
+
+    return random.choice(image_urls)
+
+
+@bot.event
+async def on_ready():
+    global error_channel
+    error_channel = bot.get_channel(error_channel_id)
+    print("Bot is ready.")
+    await bot.change_presence(activity=discord.activity.Game(name="leaked.cx"))
+
+    # Schedule the leak scraping task to run every minute
+    while True:
+        await scrape_leaks()
+        await asyncio.sleep(10)
+
+bot.run(TOKEN)
